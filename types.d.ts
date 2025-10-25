d6b4063eaf6c00cfcc69c4e1e85d99098997bad9
Austin-Wahl <austinwahl.dev@gmail.com>
2025-10-25 14:01:51 -0500

    Add types

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,5 @@
 import { Device } from 'react-native-ble-plx';
+import { BleManager } from 'react-native-ble-plx';
 export interface ITeamMember {
   name: string;
   role: string;
@@ -6,3 +7,33 @@ export interface ITeamMember {
 }
 
 export type DeviceWithLastSeen = Device & { lastSeen: number };
+
+export type TDeviceConnectionStatus =
+  | 'Connecting'
+  | 'Connected'
+  | 'Error'
+  | 'Disconnecting'
+  | 'Disconnected';
+
+export type TCharacteristics = 'read-humidity';
+
+export interface IUseBluetoothLE {
+  reconnect: (deviceId: string) => Promise<boolean>;
+  requestPermissions: () => Promise<boolean>;
+  bleManager: BleManager;
+  detectedSmartPots: Array<Device>;
+  startDeviceScan: () => Promise<void>;
+  refreshDevices: () => Promise<void>;
+  status: 'scanning' | 'finished' | 'idle' | 'error';
+  disconnect: (device: Device) => Promise<Device | null>;
+  connect: (
+    deviceId: string,
+    onConnect?: ((device: Device) => void) | null,
+    hideErrors?: boolean
+  ) => Promise<boolean>;
+  connectionStatus: TDeviceConnectionStatus;
+  connectedDevice: Device | null;
+  onConnectionDropped: (cb: (dev: Device | null) => void) => () => void;
+  SERVICE_UUID: string;
+  CHARACTERISTIC_MAP: Record<TCharacteristics, string>;
+}
